62de98acccbb844fa6e2b4dcf3d4dd7b7cbcdc47
Sarthak <huria.sarthak2@gmail.com>
2026-09-23 06:04:36 -0400

    Add incremental payment processing

--- cloud_financial_dbt/models/marts/fact_payment.sql
+++ cloud_financial_dbt/models/marts/fact_payment.sql
@@ -1,3 +1,9 @@
+{{ config(
+    materialized='incremental',
+    unique_key='payment_id',
+    incremental_strategy='merge'
+) }}
+
 SELECT
     payment_id,
     loan_id,
@@ -9,3 +15,12 @@ SELECT
     payment_status,
     days_late
 FROM {{ ref('stg_payments') }}
+
+{% if is_incremental() %}
+
+WHERE payment_id NOT IN (
+    SELECT payment_id
+    FROM {{ this }}
+)
+
+{% endif %}
